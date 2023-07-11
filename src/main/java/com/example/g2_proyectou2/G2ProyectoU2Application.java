30670fb241b478cf34c7607a31a72f219f008656
Carlos <celucio@espe.edu.ec>
2023-07-11 15:39:52 -0500

    Primer Commit

---
+++ src/main/java/com/example/g2_proyectou2/G2ProyectoU2Application.java
@@ -0,0 +1,13 @@
+package com.example.g2_proyectou2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class G2ProyectoU2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(G2ProyectoU2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/g2_proyectou2/G2ProyectoU2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.g2_proyectou2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class G2ProyectoU2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
